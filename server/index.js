8e794fe46ac2ceb2d6a19ca347593b7c6d227025
joseph slater <>
2018-06-11 23:25:36 -0500

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
+const Bean = require('ble-bean');
 const SerialPort = require('serialport');
+const BeanSerial = require('bean-serial').SerialPort;
 const Readline = SerialPort.parsers.Readline;
-const port = new SerialPort('/dev/cu.Bluetooth-Incoming-Port');
 const parser = new Readline();
-port.pipe(parser);
 
-parser.on('data', function (data) {
-  console.log(data.toString());
+var port;
+var options = {
+    logging: true // default to false
+};
+
+Bean.discoverByUuid('efff1bd8f4314c71b34b1e9547b6ab22', (bean) => {
+	bean.connectAndSetup(() => {
+		console.log('bean connected');
+	
+		port = new BeanSerial(bean, options);
+
+		bean.write(new Buffer([true]), () => { });
+		
+		port.pipe(parser);
+
+		parser.on('data', function (data) {
+      console.log(data.toString());
+      
+      bean.disconnect();
+		});
+	});
+
 });
