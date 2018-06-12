c3888939f70a71f1317f0053d71cc3a9d209ba7b
joseph slater <>
2018-06-11 23:27:55 -0500

    updates

--- server/index.js
+++ server/index.js
@@ -4,15 +4,16 @@ const BeanSerial = require('bean-serial').SerialPort;
 const Readline = SerialPort.parsers.Readline;
 const parser = new Readline();
 
+const rpiUUID = '987bf35834c6';
+const macUUID = 'efff1bd8f4314c71b34b1e9547b6ab22'
+
 var port;
 var options = {
     logging: true // default to false
 };
 
-Bean.discoverByUuid('efff1bd8f4314c71b34b1e9547b6ab22', (bean) => {
-	bean.connectAndSetup(() => {
-		console.log('bean connected');
-	
+Bean.discoverByUuid(macUUID, (bean) => {
+	bean.connectAndSetup(() => {	
 		port = new BeanSerial(bean, options);
 
 		bean.write(new Buffer([true]), () => { });
